828fe9ebb1d849314dc7b85343e03807a81d0b7e
Earlopain <earlopain@c5h8no4na.net>
2021-02-13 22:19:46 +0100

    Remove the id field from postfile The field was useless, because post_id was basically the pk anyways

--- src/main/java/net/c5h8no4na/entity/e621/PostFile.java
+++ src/main/java/net/c5h8no4na/entity/e621/PostFile.java
@@ -1,5 +1,7 @@
 package net.c5h8no4na.entity.e621;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
@@ -8,23 +10,14 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "post_file")
-public class PostFile {
+public class PostFile implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 	@Id
-	private Integer id;
-
-	private byte[] file;
-
 	@OneToOne(fetch = FetchType.LAZY)
 	private Post post;
 
-	public Integer getId() {
-		return this.id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
+	private byte[] file;
 
 	public byte[] getFile() {
 		return this.file;
