ff7ca22a05d4c59dfc9a405b16c25007dea97307
Earlopain <earlopain@c5h8no4na.net>
2021-02-13 17:18:58 +0100

    Rename source table and fix insertion Because it hat no generationtype the id would have to be set manually

--- src/main/java/net/c5h8no4na/entity/e621/Source.java
+++ src/main/java/net/c5h8no4na/entity/e621/Source.java
@@ -2,15 +2,18 @@ package net.c5h8no4na.entity.e621;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "sources")
+@Table(name = "post_sources")
 public class Source {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	private String source;
